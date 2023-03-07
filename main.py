ac1fbb4349e4c03cc1805ed9e4aaa127b9fdc7ed
Artur Siedlerski <don.siedler@gmail.com>
2023-03-07 21:51:46 +0100

    Create class

--- main.py
+++ main.py
@@ -8,10 +8,27 @@ IG_USERNAME = os.environ.get("IG_USERNAME")
 IG_PASSWORD = os.environ.get("IG_PASSWORD")
 ACCOUNT_NAME = "natgeotravel"
 
+
+class InstaFollower:
+    def __init__(self, options, service):
+        self.driver = webdriver.Chrome(service=service, options=chrome_options)
+
+    def login(self):
+        self.driver.get("https://www.instagram.com/")
+
+    def find_followers(self):
+        pass
+
+    def follow(self):
+        pass
+
+
 chrome_options = Options()
 chrome_options.add_experimental_option("detach", True)  # Keeps the browser open!
 chrome_options.add_argument("--start-maximized")
 service = Service(ChromeDriverManager().install())
 
-driver = webdriver.Chrome(service=service, options=chrome_options)
-driver.get("https://www.instagram.com/")
+bot = InstaFollower(options=chrome_options, service=service)
+bot.login()
+bot.find_followers()
+bot.follow()
